a32e3c4dee9bdf820c57dc4683f1b2228deced03
Nathi72Tcf <tcfworld144@gmail.com>
2023-08-17 23:35:56 +0200

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrBz-2NdW0HF67wKXeqlBFetxwsC6CRbw",
-    authDomain: "tcfchats.firebaseapp.com",
-    projectId: "tcfchats",
-    storageBucket: "tcfchats.appspot.com",
-    messagingSenderId: "417899635590",
-    appId: "1:417899635590:web:5ba53f101367c5bdb84207",
-    measurementId: "G-V0NDYM7KDV"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
